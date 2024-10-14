f2d5033c220b7026f35fa20da5201bcb014ad91a
dimmestsalmon <appelnephi@gmail.com>
2024-10-14 19:37:17 -0400

    added some base functions to manipulate inventory data, created basic function to calculate if inventory needs to be ordered and how much

---
+++ index.js
@@ -0,0 +1,47 @@
+let inventory  ={ 
+	shirt1:{
+	color: "grey",
+	size: "small",
+	brand: "gildan",
+	quantity: 4
+	},
+	shirt2:{
+        color: "black",
+        size: "small",
+        brand: "bella",
+        quantity: 5
+        },
+}
+
+let needed = {
+
+	shirt1:{
+	color: "grey",
+	size: "small",
+	brand: "gildan",
+	quantity: 1 
+	}
+}
+
+function searchInventory(){
+	//take input return all matches
+}
+
+function printInventory(){
+	console.log(inventory);
+}
+
+function showInventoryToPurchase(){
+	//take invnetory needed compare to on hand print difference
+}
+
+function inventoryNeeded(){
+	let orderOrOnHand;
+	if (needed.shirt1.quantity < inventory.shirt1.quantity){
+		let extra = (inventory.shirt1.quantity - needed.shirt1.quantity)
+		orderOrOnHand = `You have ${inventory.shirt1.quantity} on hand, order ${extra} amount.`
+	}
+	return orderOrOnHand;
+}
+
+console.log(inventoryNeeded());
