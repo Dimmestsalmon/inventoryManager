28b5c788a9765a23d37b11df12527f6ee3bb8526
dimmestsalmon <appelnephi@gmail.com>
2024-12-06 16:52:09 -0500

    end of week

---
+++ ui/Components/compareToOrder.jsx
@@ -0,0 +1,14 @@
+import {Link} from 'react-router-dom'
+
+const CompareToOrder = () => {
+
+
+  return(
+    <>
+    <h1>What to order</h1>
+    <Link to = '/'>Home</Link>
+    </>
+  )
+}
+
+export default CompareToOrder
--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
 import './App.css'
 import InventoryList from '../Components/inventoryList.jsx'
 import AddInventory from '../Components/addInventory.jsx'
+import CompareToOrder from '../Components/compareToOrder.jsx'
 
 function App() {
 
@@ -18,10 +19,12 @@ function App() {
                 <h1>Inventory Tracker</h1>
                 <Link to="/inventoryList">Show Inventory</Link>
                 <Link to="/addInventory">Add Inventory</Link>
+                <Link to="/compareToOrder">Compare To Order</Link>
               </>
           }/>
           <Route path="/inventoryList" element={<InventoryList />} />
           <Route path="/addInventory" element={<AddInventory />} />
+          <Route path="/compareToOrder" element={<CompareToOrder />} />
         </Routes>
       </Router>
     </>
