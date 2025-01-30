cd691d296c28ace7f23608308e73250e2b4dafe9
Nephi Appel <dimmestsalmon@Nephis-MacBook-Pro.local>
2025-01-30 10:11:10 -0500

    removed cors as it failed on prod

--- server/server.js
+++ server/server.js
@@ -2,13 +2,13 @@ const express = require('express');
 const server = express();
 const port = 8080;
 const knex = require('knex')(require('./knexfile.js')['production']);
-const cors = require('cors');
+/*const cors = require('cors');
 server.use(express.json())
 server.use(
   cors({
     origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
   })
-);
+);*/
 server.get('/', (req, res) => res.send('Hello Cici!'))
 
 server.get('/inventoryList', (req, res) => {
