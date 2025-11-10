ffb10b08ee2bc641bb19615500e9e656dcc52b79
NephiAppel <appelnephi@gmail.com>
2025-11-10 07:45:32 -0500

    added functionality to delete button

--- src/app/inventoryList/page.tsx
+++ src/app/inventoryList/page.tsx
@@ -2,8 +2,9 @@ import { neon } from "@neondatabase/serverless";
 import Link from "next/link";
 
 export default async function InventoryList() {
+  const sql = neon(process.env.DATABASE_URL!);
+
   async function getData() {
-    const sql = neon(process.env.DATABASE_URL!);
     const list = await sql("SELECT * FROM inventory");
     return list as {
       id: number;
@@ -16,6 +17,17 @@ export default async function InventoryList() {
     }[];
   }
 
+async function removeItem(formData: FormData) {
+    'use server';
+    
+    const id = formData.get('id') as string;
+    
+    try {
+      await sql("DELETE FROM inventory WHERE id = $1", [id]);
+    } catch {
+      console.log("Remove item failed");
+    }
+  }
   const inventoryList: { id: number; brand: string; color: string; style: string; size: string; quantity: number; location: string  }[] = await getData();
 
   return (
@@ -25,7 +37,7 @@ export default async function InventoryList() {
           {inventoryList.map((item: { id: number; brand: string; color: string; style: string; size: string; quantity: number; location: string  }) => (
               <li key={item.id}>
                 {item.brand}{item.color}{item.style}{item.size}{item.quantity}{item.location}
-                <button type = "button"> remove </button>
+                <button type = "button" onClick={removeItem}> remove </button>
               </li>
               
           ))}
